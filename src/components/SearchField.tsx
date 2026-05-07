c3abf87a9d38c6fb0382581637c80f734b970795
Amit <hazeydays@gmail.com>
2026-05-07 10:47:54 -0700

    added debounced search

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -1,18 +1,31 @@
+import { useEffect, useState } from "react";
 import { handleSearch } from "../lib/utils";
-import type { CampaignFilters } from "../types";
 
-const SearchField = ({ filters }: { filters: CampaignFilters }) => {
+const SearchField = () => {
+  const [query, setQuery] = useState<string>("");
+  const [debouncedQuery, setDebouncedQuery] = useState<string>("");
 
-  // to do - add debounce
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedQuery(query);
+    }, 400);
 
+    return () => clearTimeout(timerId);
+  }, [query]);
+
+  useEffect(() => {
+    handleSearch(debouncedQuery);
+  }, [debouncedQuery]);
 
   return (
     <input
       type="text"
-      value={filters.search}
+      value={query}
       placeholder="Search campaigns..."
       className="px-3 py-2 border border-gray-400 rounded-lg text-lg flex-1 max-w-xs focus:outline-none"
-      onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSearch(e.target.value)}
+      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+        setQuery(e.target.value)
+      }
     />
   );
 };
